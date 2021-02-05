1cabaae0665c6d9adba361d17eae06e1bced90f1
Sittiphol Yooyod <20336055+yooyod@users.noreply.github.com>
2021-02-05 16:45:36 +0700

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -3,13 +3,14 @@
  * Use this file to define custom functions and blocks.
  * Read more at https://makecode.microbit.org/blocks/custom
  */
-
+/*
 enum MyEnum {
     //% block="one"
     One,
     //% block="two"
     Two
 }
+*/
 
 /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   ++                            Custom blocks                     ++
